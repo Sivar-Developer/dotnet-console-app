6a4a98075d2200b587527931fed44038fb224ca5
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2019-01-18 15:27:11 +0300

    Initialize hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From My Console App!");
+        }
+    }
+}
